a2224423185a05fef287f11e9ea5cd4952abfed7
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-08-07 05:07:19 +0200

    Add the landing page and its main picture, add css files

---
+++ views/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- SEO -->
+    <meta name="description" content="Billet simple pour l'Alaska">
+    <meta name="keyworks" content="livre, Alaska, Forteroche">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" href="assets/css/reset.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Lobster" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <!-- <script src="assets/js/scripts.js"></script> -->
+    
+    <title>Billet simple pour l'Alaska, par Jean Forteroche</title>
+</head>
+    
+<body>
+    <div id='wrapper'>
+        <header>
+            <h1>Billet simple pour l'Alaska</h1>
+            <h2>le nouveau roman de Jean Forteroche</h2>
+        </header>
+        
+        <section>
+            <article>
+                <p>"Tout juste diplômé, Christopher McCandless, 22 ans, est promis à un brillant avenir. Pourtant, tournant le dos à l'existence confortable et sans surprise qui l'attend, le jeune homme décide de prendre la route en laissant tout derrière lui. Des champs de blé du Dakota aux flots tumultueux du Colorado, en passant par les communautés hippies de Californie, Christopher va rencontrer des personnages hauts en couleur. Chacun, à sa manière, va façonner sa vision de la vie et des autres."</p>
+                <a href="blog.php">Sélection des chapitres</a>
+            </article>
+        
+            <aside>
+                <p>Diplômé d'état en alpinisme, puis en climatologie, Jean Forteroche est un écrivain et chercheur français. Son premier roman, "<em>Les neiges d'été</em>" (Editions Pocket), s'est vendu à plus d'un demi million d'exemplaires dans le monde et a été traduit en plus de 20 langues. Depuis, il a écrit "<em>Le paradis du Nord</em>" (2010), "<em>L'appel de la montagne</em>" (2012) et "<em>L'étranger d'ici</em>" (2014), qui ont tous publiés dans le monde entier. "<em>Billet simple pour l'Alaska</em>" est, pour lui, un hommage rendu au peuple autochtones qu'il a pu rencontrer au cours de ses nombreuses excursions dans les terres du soleil de minuit.</p>
+            </aside>
+        </section>
+        
+        <footer>
+            <ul>
+                <li><a href="http://twitter.com/realjforteroche">Twitter</a></li>
+                <li><a href="http://facebook.com/jean.forteroche">Facebook</a></li>
+            </ul>
+            <p>©2018 Wild Nature Discovery Library</p> 
+        </footer>
+    </div>
+</body>
