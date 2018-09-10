0dd7147377b7f46a2502e1e5295cb550e4d7433d
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-09-10 19:39:43 +0200

    add the previous burger menu to all currently used nav bars

--- view/backend/chapterEditing.php
+++ view/backend/chapterEditing.php
@@ -1,10 +1,27 @@
 <div class="admin-session-bar">
+    <div>
+        <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
+        <label for="openSidebarMenu" class="sidebarIconToggle">
+            <div class="spinner diagonal part-1"></div>
+            <div class="spinner horizontal"></div>
+            <div class="spinner diagonal part-2"></div>
+        </label>
+
+        <div id="sidebarMenu">
+            <ul class="sidebarMenuInner">
+                <li>
+                    <button class="light-blue-button white-border"><a href="index.php?action=getAdminPanel">Tableau de bord</a></button>
+                </li>
+                <li>
+                    <button class="white-button dark-blue-border"><a href="index.php">Voir mon site</a></button>
+                </li>
+                <li>
+                    <button class="orange-button white-border"><a href="index.php">Déconnexion</a></button>
+                </li>
+            </ul>
+        </div>
+    </div>
     <p>Bienvenue sur le tableau de bord !</p>
-    <nav>
-        <button class="light-blue-button white-border"><a href="index.php?action=getAdminPanel">Tableau de bord</a></button>
-        <button class="white-button dark-blue-border"><a href="index.php">Voir mon site</a></button>
-        <button class="orange-button white-border"><a href="index.php">Déconnexion</a></button>
-    </nav>
 </div>
 
 <div class="admin-panel-wrapper">
--- view/frontend/adminBar.php
+++ view/frontend/adminBar.php
@@ -1,8 +1,25 @@
-<div class="user-session-bar">
-    <p>Bienvenue, Pierre, sur le portail du nouveau livre de Jean Forteroche.</p>
-    <nav>
-        <button class="light-blue-button white-border"><a href="index.php?action=getAdminPanel">Panneau d'administration</a></button>
-        <button class="white-button light-blue-border"><a href="index.php?action=getMemberPanel&amp;id=1">Espace membre</a></button>
-        <button class="orange-button white-border"><a href="index.php?action=signOut">Déconnexion</a></button>
-    </nav>
+<div class="admin-session-bar">
+    <div>
+        <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
+        <label for="openSidebarMenu" class="sidebarIconToggle">
+            <div class="spinner diagonal part-1"></div>
+            <div class="spinner horizontal"></div>
+            <div class="spinner diagonal part-2"></div>
+        </label>
+
+        <div id="sidebarMenu">
+            <ul class="sidebarMenuInner">
+                <li>
+                    <button class="light-blue-button white-border"><a href="index.php?action=getAdminPanel">Panneau d'administration</a></button>
+                </li>
+                <li>
+                    <button class="white-button light-blue-border"><a href="index.php?action=getMemberPanel&amp;id=1">Espace membre</a></button>
+                </li>
+                <li>
+                    <button class="orange-button white-border"><a href="index.php?action=signOut">Déconnexion</a></button>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p>Bonne lecture, Pierre !</p>
 </div>
